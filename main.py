8f3b199e6523d8bf50196603b50d169b4f112c1a
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-03-25 18:17:23 +0100

    Adding files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import missingno as msn
+import seaborn as sbn
+import scapy
+import scikitplot
+import bs4
+import networkx
+import boto3
+import matplotlib.pyplot as plt
